ee3cd947d6941ca8a47285984708e818237c651c
Leo Choosue <liuchushu10@gmail.com>
2016-06-20 08:33:51 +0800

    finished pre process

--- spamassasin.m
+++ spamassasin.m
@@ -3,11 +3,11 @@
 %  the email to make our following steps easiler.
 %
 file_path_array = {"./spamassasin-dataset/easy_ham/", 
-                   "./spamassasin-dataset/easy_ham_2", 
+                   "./spamassasin-dataset/easy_ham_2/", 
                    "./spamassasin-dataset/hard_ham/", 
                    "./spamassasin-dataset/spam/", 
                    "./spamassasin-dataset/spam_2/"};
 iter_array = [2500, 1400, 250, 500, 1400];
-for i = 1:5
+for i = 2:5
     preProcess(file_path_array{i}, iter_array(i));
 end
